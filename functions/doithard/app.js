b02f675eaa3289a310e12e5254ebf038593d0c48
hiraro <traurig.orz@gmail.com>
2017-02-07 01:33:30 +0900

    update20170207

--- functions/doithard/app.js
+++ functions/doithard/app.js
@@ -26,12 +26,10 @@ exports.handler = (event, context, callback) => {
     const user_id = process.env.AIPO_USERID;	
     const passwd = process.env.AIPO_PASSWD;
     const target_aipo_url = getAipoUrlFromInstanceId(event.pathParameters.instanceId);
-    const target_username = decodeURIComponent(event.pathParameters.targetUsername);
 
     console.log(event);
     console.log(context);
     console.log(target_aipo_url);
-    console.log(target_username);
 
     phantomjs
         .run('--webdriver=4444')
@@ -48,25 +46,8 @@ exports.handler = (event, context, callback) => {
                 .setValue('#password', passwd)
                 .click('.button[value="ログイン"]')
                 .waitForExist('=Aipo', 10000)
-                .$$(`//div[@id='timelineOuter']//div[@class='messageContents' and .//a[contains(text(),'${target_username}')]]//a[text()='いいね！']`)
-                .then((elems) => {
-                  const ps = elems.map((elem) => {
-                    return client
-                      .elementIdClick(elem.ELEMENT);
-                  });
-                  return Promise.all(ps);
-                })
-                .$$(`//div[@id='timelineOuter']//div[@class='messageContents' and .//a[contains(text(),'${target_username}')]]`)
-                .then((elems) => {
-                  const ps = elems.map((elem) => {
-                    return client
-                      .elementIdText(elem.ELEMENT)
-                      .then((res) => {
-                        console.log(res.value);
-                      });
-                  });
-                  return Promise.all(ps);
-                })
+                .click('//input[@type="button" and (@value="出勤" or @value="退勤")]')
+                .pause(5000)
                 .then(() => {
                     phantom.kill();
                     return Promise.resolve();
@@ -74,7 +55,7 @@ exports.handler = (event, context, callback) => {
                 .catch((err) => {
                     console.error(`error: ${err}`);
                     phantom.kill();
-                    return Promise.reject(err);
+                    return Promise.reject(err);  
                 });
         })
         .then(() => {
@@ -85,7 +66,7 @@ exports.handler = (event, context, callback) => {
             });
         })
         .catch((err) => {
-            console.error(`failed:${err}`);
+            console.error(`failed: ${err}`);
             context.fail({
                 statusCode: 500,
                 body: JSON.stringify({ "message": `failed:${err}` })
